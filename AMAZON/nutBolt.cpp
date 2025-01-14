8871bf49726769ef36a268e0433cf434f007408e
Mdeepak0 <114018823+Mdeepak0@users.noreply.github.com>
2025-01-15 01:27:30 +0530

    Create nutBolt.cpp

---
+++ AMAZON/nutBolt.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+
+    void matchPairs(int n, char nuts[], char bolts[]) {
+        char temp[]= { '!','#','$','%','&','*','?','@','^'};
+        unordered_map<char,int> mp;
+        int j = 0;
+        
+        for(int i =0;i<n;i++) mp[nuts[i]]++;
+        for(int i=0;i<9;i++){
+            if(mp[temp[i]]){
+                nuts[j] = temp[i];
+                bolts[j] = temp[i];
+                j++;
+            }
+        }
+    }
+};
